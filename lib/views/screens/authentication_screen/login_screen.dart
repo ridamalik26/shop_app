1fc3fbca57e618c50d13f3fc2b0c78f908379eef
ridamalik26 <malikrida406@gmail.com>
2025-11-23 02:41:48 +0500

    Initial commit

---
+++ lib/views/screens/authentication_screen/login_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Login Your Account",
+              style: GoogleFonts.getFont(
+                  'Lato',
+                  color: Color(0xFF0d120E),
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 0.2,
+                  fontSize: 23
+              ),),
+            Text(
+              'To Explore the world exclusive',
+              style: GoogleFonts.getFont(
+                  'Lato',
+                  color: Color(0xFF0d120E),
+                  fontSize: 14,
+                  letterSpacing: 0.2,
+              ),
+            ),
+            Image.asset('assets/images/')
+          ],
+        ),
+      ),
+    );
+  }
+}
