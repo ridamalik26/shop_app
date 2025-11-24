4c240a0d3ec64668b21c2862397017bce8461f58
ridamalik26 <malikrida406@gmail.com>
2025-11-24 20:56:07 +0500

    Completed UI of login & signup page

--- lib/views/screens/authentication_screen/login_screen.dart
+++ lib/views/screens/authentication_screen/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shop_app/views/screens/authentication_screen/register_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -7,29 +8,151 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Login Your Account",
-              style: GoogleFonts.getFont(
-                  'Lato',
-                  color: Color(0xFF0d120E),
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.2,
-                  fontSize: 23
-              ),),
-            Text(
-              'To Explore the world exclusive',
-              style: GoogleFonts.getFont(
-                  'Lato',
-                  color: Color(0xFF0d120E),
-                  fontSize: 14,
-                  letterSpacing: 0.2,
+      backgroundColor: Colors.grey.shade200,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Login Your Account",
+                style: GoogleFonts.getFont(
+                    'Lato',
+                    color: Color(0xFF0d120E),
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.2,
+                    fontSize: 23
+                ),),
+              Text(
+                'To Explore the world exclusive',
+                style: GoogleFonts.getFont(
+                    'Lato',
+                    color: Color(0xFF0d120E),
+                    fontSize: 14,
+                    letterSpacing: 0.2,
+                ),
               ),
-            ),
-            Image.asset('assets/images/')
-          ],
+              Image.asset('assets/images/Illustration.png',
+                width: 200,
+                height: 200,),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Text('Email',
+                  style: GoogleFonts.getFont(
+                  'Nunito Sans',
+                  fontWeight: FontWeight.bold,
+                  letterSpacing:  0.2,
+                  ),),
+              ),TextFormField(
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(9),
+                  ),
+                  focusedBorder: InputBorder.none,
+                  enabledBorder: InputBorder.none,
+                  labelText: 'enter your email',
+                  labelStyle: GoogleFonts.getFont(
+                    'Nunito Sans',
+                    fontSize: 14,
+                    letterSpacing: 0.1,
+                  ),
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset('assets/icons/email.png',
+                    width: 20,
+                      height: 20 ,
+                    ),
+                  )
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(9),
+                    ),
+                    focusedBorder: InputBorder.none,
+                    enabledBorder: InputBorder.none,
+                    labelText: 'enter your password',
+                    labelStyle: GoogleFonts.getFont(
+                      'Nunito Sans',
+                      fontSize: 14,
+                      letterSpacing: 0.1,
+                    ),
+                    prefixIcon: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Image.asset('assets/icons/password.png',
+                        width: 20,
+                        height: 20 ,
+                      ),
+                    ),
+                  suffixIcon: Icon(Icons.visibility),
+                ),
+              ),
+
+              SizedBox(
+                height: 30,
+              ),
+
+              Container(
+                width: 319,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  gradient: LinearGradient(
+                      colors: [
+                        Color(0xFF192DE1),
+                        Color(0xCC0D6EFF)
+                      ],
+                  ),
+                ),
+                child: Center(
+                    child: Text(
+                        'Sign in',
+                      style: GoogleFonts.getFont(
+                          'Lato',
+                          fontSize: 17,
+                          color: Colors.white
+                      ),
+                    ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Need an Account?',
+                    style: GoogleFonts.roboto(
+                        fontWeight: FontWeight.bold,
+                    letterSpacing: 1,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context, MaterialPageRoute(builder: (context){
+                            return RegisterScreen();
+                      }));
+                    },
+                    child: Text(
+                      'Sign Up',
+                      style: GoogleFonts.roboto(color: Color(0xFF192DE1),
+                      fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
