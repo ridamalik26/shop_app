4c1c4fa85f6781effd817f19f83b1883941511dc
ridamalik26 <malikrida406@gmail.com>
2025-11-29 21:47:59 +0500

    worked on home screen, added a searchBarr and different icons

--- lib/views/screens/nav_screen/home_screen.dart
+++ lib/views/screens/nav_screen/home_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/views/screens/nav_screen/widgets/header_widget.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('Home Screen'),),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            HeaderWidget(),
+          ],
+        ),
+      )
     );
   }
 }
