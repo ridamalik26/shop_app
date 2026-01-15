95238bd55010310203e1bbd1a0e76d9732881c89
ridamalik26 <malikrida406@gmail.com>
2026-01-15 23:15:39 +0500

    created reusable widget

--- lib/views/screens/nav_screen/widgets/category_item_widget.dart
+++ lib/views/screens/nav_screen/widgets/category_item_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:shop_app/views/screens/nav_screen/widgets/reusable_text_widget.dart';
 
 import '../../../../controllers/category_controller.dart';
 import '../../../../models/category.dart';
@@ -23,44 +24,50 @@ class _CategoryWidgetState extends State<CategoryWidget> {
   }
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: futureCategories,
-        builder: (context, snapshot){
-          if(snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator(),
-            );
-          }else if(snapshot.hasError){
-            return Center(child: Text('Error ${snapshot.error}'),
-            );
-          }else if(!snapshot.hasData || snapshot.data!.isEmpty){
-            return const Center(child: Text('No Categories'),);
-          }else{
-            final categories = snapshot.data!;
-            return GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: categories.length ,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                  mainAxisSpacing: 8,
-                  crossAxisSpacing: 8,
-                ),
-                itemBuilder: (context, index){
-                  final category = categories[index];
-                  return Column(
-                    children: [
-                      Image.network(category.image,
-                        height: 47,
-                        width: 47,
-                      ),
-                      Text(category.name, style: GoogleFonts.quicksand(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
-                      ),),
-                    ],
-                  );
-                });
-          }
-        });
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const ReusableTextWidget(title: 'Categories', subtitle: 'View all'),
+        FutureBuilder(
+            future: futureCategories,
+            builder: (context, snapshot){
+              if(snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator(),
+                );
+              }else if(snapshot.hasError){
+                return Center(child: Text('Error ${snapshot.error}'),
+                );
+              }else if(!snapshot.hasData || snapshot.data!.isEmpty){
+                return const Center(child: Text('No Categories'),);
+              }else{
+                final categories = snapshot.data!;
+                return GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: categories.length ,
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4,
+                      mainAxisSpacing: 8,
+                      crossAxisSpacing: 8,
+                    ),
+                    itemBuilder: (context, index){
+                      final category = categories[index];
+                      return Column(
+                        children: [
+                          Image.network(category.image,
+                            height: 47,
+                            width: 47,
+                          ),
+                          Text(category.name, style: GoogleFonts.quicksand(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),),
+                        ],
+                      );
+                    });
+              }
+            }),
+      ],
+    );
   }
 }
