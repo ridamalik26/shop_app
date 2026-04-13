0aabd14cbca3fc29b3cf092a184a4142d9537067
ridamalik26 <malikrida406@gmail.com>
2026-04-14 01:02:35 +0500

    diplayed cart items

--- lib/views/screens/nav_screen/cart_screen.dart
+++ lib/views/screens/nav_screen/cart_screen.dart
@@ -9,7 +9,7 @@ class CartScreen extends ConsumerStatefulWidget {
   _CartScreenState createState() => _CartScreenState();
 }
 
-class _State extends ConsumerState<CartScreen> {
+class _CartScreenState extends ConsumerState<CartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,9 +19,46 @@ class _State extends ConsumerState<CartScreen> {
             height: 118,
             clipBehavior: Clip.hardEdge,
             decoration: BoxDecoration(
-              image: DecorationImage(image: AssetImage('assets/icons/cartb.png',),
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/icons/cartb.png',
+                ),
               fit: BoxFit.cover,
               ),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 322,
+                    top: 52,
+                    child: Stack(
+                      children: [
+                        Image.asset('assets/icons/not.png',
+                        width: 25,
+                          height: 25,
+                        ),
+                        Positioned(
+                            top: 0,
+                            right: 0,
+                            child: Container(
+                              width: 20,
+                              height: 20,
+                              padding: const EdgeInsets.all(4),
+                              decoration: BoxDecoration(
+                                color: Colors.yellow.shade800,
+                                borderRadius: BorderRadius.circular(
+                                  12,
+                                ),
+                              ),
+                              child: Center(
+                                child: Text('1', style: TextStyle() ,),
+                              ),
+                            ),
+                        ),
+                      ],
+                    ),
+                )
+              ],
             ),
           ),
       ),
