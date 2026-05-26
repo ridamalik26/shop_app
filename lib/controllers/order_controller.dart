9fdce507939ff5bd9b1c63a966f23a23767d439a
ridamalik26 <malikrida406@gmail.com>
2026-05-26 15:39:24 +0500

    Get Orders - Frontend

--- lib/controllers/order_controller.dart
+++ lib/controllers/order_controller.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:shop_app/models/order.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop_app/services/manage_http_response.dart';
@@ -22,7 +25,7 @@ class OrderController {
     required String vendorId,
     required bool processing,
     required bool delivered,
-    required context,
+    required BuildContext context,
 }
 )async
   {
@@ -50,10 +53,41 @@ class OrderController {
             "Content-Type": 'application/json; charset=UTF-8'
               },
       );
-      manageHttpResponse(
-          response: response, context: context, onSuccess: () {});
+      if (context.mounted) {
+        manageHttpResponse(
+            response: response, context: context, onSuccess: () {});
+      }
     }catch (e){
-      showSnackBar(context, e.toString());
+      if (context.mounted) {
+        showSnackBar(context, e.toString());
+      }
+    }
+  }
+
+  //Method to get orders by buyer id
+
+  Future<List<Order>> loadOrders({required String buyerId})async{
+    try{
+      //Send an HTTP GET req to get the orders by the buyerID
+      http.Response response = await http.get(Uri.parse('$uri/api/orders/$buyerId'),
+        headers: <String, String>{
+          "Content-Type": 'application/json; charset=UTF-8'
+        },
+      );
+      //check the response status code is 200(0)
+      if(response.statusCode==200){
+        //parse the json response body into dynamic list
+        //this convert the json data into a format that can be further processed in dart
+        List<dynamic> data = jsonDecode(response.body);
+        //map the dynamic list to list to orders object using the form json factory method
+        //this step converts the raw data into list of the orders instance, which are easier to work with
+        List<Order> orders = data.map((order)=> Order.fromJson(order)).toList();
+        return orders;
+      } else {
+        throw Exception("failed to load Orders");
+      }
+    } catch (e) {
+        throw Exception('error Loading Orders: $e');
     }
   }
 }
