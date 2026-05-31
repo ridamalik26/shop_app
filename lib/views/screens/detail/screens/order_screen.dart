15e14eebcec496cb7f959b2fdb82b66c8ce7bbd9
ridamalik26 <malikrida406@gmail.com>
2026-05-31 22:52:43 +0500

    display orders

--- lib/views/screens/detail/screens/order_screen.dart
+++ lib/views/screens/detail/screens/order_screen.dart
@@ -41,8 +41,74 @@ class _OrderScreenState extends ConsumerState<OrderScreen> {
               itemCount: orders.length,
               itemBuilder: (context, index){
             final Order order = orders[index];
-            return ListTile(
-              title: Text(order.productName),
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 25 ),
+              child: Container(
+                width: 335,
+                height: 153,
+                clipBehavior: Clip.antiAlias,
+                decoration: const BoxDecoration(),
+                  child: SizedBox(
+                  width: double.infinity,
+                    child: Stack(
+                      clipBehavior: Clip.none,
+                      children: [
+                        Positioned(
+                          left: 0,
+                          top: 0,
+                          child: Container(
+                            width: 336,
+                            height: 154,
+                            clipBehavior: Clip.antiAlias,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              border: Border.all(
+                                color: const Color(
+                                  0xFFEFF0F2,
+                                )
+                              ),
+                              borderRadius: BorderRadius.circular(9,)
+                            ),
+                            child: Stack(
+                              clipBehavior: Clip.none,
+                              children: [
+                                Positioned(
+                                    left: 13,
+                                    top: 9,
+                                    child: Container(
+                                      width: 78,
+                                      height: 78,
+                                      clipBehavior: Clip.antiAlias,
+                                      decoration: BoxDecoration(
+                                        color: Color(
+                                          0xFFBCC5FF
+                                        ),
+                                        borderRadius: BorderRadius.circular(8),
+                                      ),
+                                      child: Stack(
+                                        clipBehavior: Clip.none,
+                                        children: [
+                                          Positioned(
+                                              left: 10,
+                                              top: 5,
+                                              child: Image.network(order.image,
+                                                width: 58,
+                                                height: 67,
+                                                fit: BoxFit.cover,
+                                              ),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                ),
+              ),
             );
           })
     );
