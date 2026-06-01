66a2278dbb786dcea3edf34f5222ee798146b473
ridamalik26 <malikrida406@gmail.com>
2026-06-01 20:43:42 +0500

    custom app bar

--- lib/views/screens/detail/screens/order_screen.dart
+++ lib/views/screens/detail/screens/order_screen.dart
@@ -37,6 +37,71 @@ class _OrderScreenState extends ConsumerState<OrderScreen> {
   Widget build(BuildContext context) {
     final orders = ref.watch(orderProvider);
     return Scaffold(
+        appBar: PreferredSize(preferredSize: Size.fromHeight(MediaQuery.of(context).size.height *0.20),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: 118,
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/icons/cartb.png',
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  left: 322,
+                  top: 52,
+                  child: Stack(
+                    children: [
+                      Image.asset('assets/icons/not.png',
+                        width: 25,
+                        height: 25,
+                      ),
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: Container(
+                          width: 20,
+                          height: 20,
+                          padding: const EdgeInsets.all(4),
+                          decoration: BoxDecoration(
+                            color: Colors.yellow.shade800,
+                            borderRadius: BorderRadius.circular(
+                              12,
+                            ),
+                          ),
+                          child: Center(
+                            child: Text(orders.length.toString() ,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 11,
+                              ) ,),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                    left: 61,
+                    top: 51,
+                    child: Text('My Orders',
+                      style: GoogleFonts.montserrat(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    )
+                )
+              ],
+            ),
+          ),
+        ),
       body: orders.isEmpty?Center(child: Text('No Order Found'),):
           ListView.builder(
               itemCount: orders.length,
@@ -192,6 +257,19 @@ class _OrderScreenState extends ConsumerState<OrderScreen> {
                                       ),
                                   ),
                                 ],
+                              ),
+                            ),
+                        ),
+                        Positioned(
+                            top: 115,
+                            left: 298,
+                            child: InkWell(
+                              onTap: (){
+
+                              },
+                              child: Image.asset('assets/icons/delete.png',
+                              width: 20,
+                                height: 20,
                               ),
                             ),
                         ),
